ed416436976a7920fca561e822c1c2bcb292a6a7
Zahir Nikraftar <39174566+zahirnik@users.noreply.github.com>
2025-05-18 12:14:59 +0100

    Update Model_submit.sh

--- Model-Code/Model_submit.sh
+++ Model-Code/Model_submit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #$ -cwd
 #$ -j y
-#$ -pe smp 24       # 16 cores (8 cores per GPU)
+#$ -pe smp 24       # 24 cores (12 cores per GPU)
 #$ -l h_rt=10:0:0    # 1 hour runtime (required to run on the short queue)
 #$ -l h_vmem=11G    # 11 * 16 = 176G total RAM
 #$ -l gpu=2         # request 2 GPUs
@@ -13,7 +13,7 @@ module load python
 python3 -m venv /data/Environment/pytorchenv
 
 module load miniconda
-#source /data/EECS-Theory/Clim_risk_Lab_Zahir_Rendani/Virtual-Env/pytorchenv/bin/activate
+#source /data/Virtual-Env/pytorchenv/bin/activate
 source /data/pytorchenv/bin/activate
 
 pip install dask
@@ -33,4 +33,4 @@ pip install scipy
 pip install scikit-learn
 pip install -U scikit-learn
 
-python /data/UK/code/run.py
+python /data/run.py
